c574dc741730d3b480f46841ef2b32c4a16ea011
Jimmy <r0913415@student.thomasmore.be>
2022-12-18 17:02:22 +0100

    update project

--- js/owl.js
+++ js/owl.js
@@ -23,17 +23,3 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
-
-
-var owl = $('.owl-carousel');
-owl.owlCarousel();
-// Go to the next item
-$('.customNextBtn').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-// Go to the previous item
-$('.customPrevBtn').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [300]);
-})
